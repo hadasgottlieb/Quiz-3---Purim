c5a54d1ef9b6fe49f88de3d4b77487ea4deec25b
Hadas Gottlieb <hadasgottlieb194@gmail.com>
2020-02-27 00:29:32 -0500

    Added submit function

---
+++ README.md
@@ -0,0 +1,4 @@
+PRACTICE
+This is a quiz about Purim.
+
+This application: A quiz on Purim. Quiz begins with opening div introducing the quiz. Below that is button that when pressed, starts the quiz. Question, 4 choices, Submit button. When choice is made and submit button is pressed - if answer is correct, say correct. If answer is incorrect, say incorrect, and give correct answer. After Submit button is pressed, Next button appears. Press Next to go to next question. If no more questions, display results of quiz, and Start Over button.
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
             a. Zeresh *
             b. Cindy
             c. Haman had no wife.
-            d. Haman's wife had no nose.
+            d. Haman's wife had no husband.
         2. How were Esther and Mordechai related?
             a. They were the same person.
             b. Esther was Mordechai's accountant.
@@ -34,7 +34,7 @@ let STORE = {
     {
         questionNumber: 1,
         question: "Who was Haman's wife?",
-        options: ["Zeresh", "Cindy", "Haman had no wife.", "Haman's wife had no nose."],
+        options: ["Zeresh", "Cindy", "Haman had no wife.", "Haman's wife had no husband."],
         correctAnswer: "Zeresh",
     },
     {
@@ -66,16 +66,51 @@ let STORE = {
 //This function renders the quiz
 function startQuiz() {
     console.log("function startQuiz ran.");
+
+    $("#start-button").on("click", function() {
+        $("#opening-container").hide();
+        $("#quiz-container").removeClass("hide-on-start");
+
+        buildQuiz();
+    });
 }
 
 //This function builds the quiz HTML
 function buildQuiz() {
     console.log("function buildQuiz ran.");
+
+    let currentQ = STORE.questions[STORE.currentQuestionIndex];
+    $("#question-container").html("");
+    $("#options-container").html("");
+    $("#question-container").html(`<h2>${currentQ.questionNumber}. ${currentQ.question}</h2>`);
+    currentQ.options.forEach(function(option) {
+        $("#options-container").append(`
+        <div>
+            <label>
+                <input type = "radio" name = "radio" value = "${option}" >
+                <span>${option}</span>
+            </label>
+        </div>`);
+    });
+
 }
 
 //This function listens for Submit button, compares to correct answer, tallies correct/incorrect
 function submitAnswer() {
     console.log("function submitAnswer ran.");
+
+    $("#submit-button").on("click", function() {
+        $("#submit-button").hide();
+        let userGuess = $("input:checked").val();
+        if (userGuess) {
+            if (userGuess === STORE.questions[STORE.currentQuestionIndex].correctAnswer) {
+                $("#output").text(`Correct!`);
+                STORE.correctCount++;
+            } else {
+                $("#output").text(`Incorrect. The correct answer is "${STORE.questions[STORE.currentQuestionIndex].correctAnswer}".`);
+            }
+        }
+    });
 }
 
 //This function listens for the Next button, renders next question
